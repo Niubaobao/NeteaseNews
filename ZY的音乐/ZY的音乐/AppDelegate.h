3cf960208b42d58ebf384b47de1d7dbfffc58f16
yachao <451142214@qq.com>
2015-10-30 18:40:37 +0800

    初始化

---
+++ ZY的音乐/ZY的音乐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZY的音乐
+//
+//  Created by 张亚超 on 15/10/30.
+//  Copyright © 2015年 zhaoyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZY的音乐/ZY的音乐/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZY的音乐
+//
+//  Created by 张亚超 on 15/10/30.
+//  Copyright © 2015年 zhaoyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
